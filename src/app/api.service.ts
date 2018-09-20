d8e8363b7b504a6c151294918701747a7f9dab5f
oxenprogrammer <emmy1000okello@gmail.com>
2018-09-20 10:12:43 +0300

    Feature[#160618214 Fixes] add placeholder api CRUD methods

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { environment } from './../environments/environment';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Job } from './job';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +10,30 @@ export class ApiService {
 
   // Inject the in-built http innto the constructor
   constructor(private http: Http) { }
+
+  // API: GET /jobss
+  public getAllJobs() {
+    // will use this.http.get()
+  }
+
+  // API: POST /jobs
+  public createJob(job: Job) {
+    // will use this.http.post()
+  }
+
+  // API: GET /jobs/:id
+  public getJobById(jobId: number) {
+    // will use this.http.get()
+  }
+
+  // API: PUT /jobs/:id
+  public updateJob(job: Job) {
+    // will use this.http.put()
+  }
+
+  // DELETE /jobs/:id
+  public deleteJobById(jobId: number) {
+    // will use this.http.delete()
+  }
+
 }
