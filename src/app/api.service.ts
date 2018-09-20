6b32fc33d8975638893604c2868eed6cde2456f2
oxenprogrammer <emmy1000okello@gmail.com>
2018-09-20 09:30:30 +0300

    Feature[#160618214 Fixes] add the environment and http

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,12 @@
+import { environment } from './../environments/environment';
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  // Inject the in-built http innto the constructor
+  constructor(private http: Http) { }
 }
