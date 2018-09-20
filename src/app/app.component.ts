300780b374f5ed42478261a7892d4e0fe77d6998
oxenprogrammer <emmy1000okello@gmail.com>
2018-09-20 19:49:55 +0300

    Feature[#160618214 Fixes] inject JobDataService into AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { JobDataServiceService } from './job-data-service.service';
+import { Component, OnInit } from '@angular/core';
+import { Job } from './job';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [JobDataServiceService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'iYouthApp';
+
+  constructor(private jobDataService: JobDataServiceService) { }
+
+  // Define a public property job and initial it to an empty list
+  jobs: Job[] = [];
+
+  public ngOnInit() {
+    this.jobDataService
+      .getAllJobs()
+      .subscribe(
+        jobs => this.jobs = jobs
+      );
+  }
 }
