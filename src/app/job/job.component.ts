564312cbca22d97d8f7b945636ad0272d410d953
oxenprogrammer <emmy1000okello@gmail.com>
2018-09-24 09:49:26 +0300

    Feature[#160618343 Fixes] restructure app component

--- src/app/job/job.component.ts
+++ src/app/job/job.component.ts
@@ -1,4 +1,6 @@
+import { JobDataServiceService } from './../job-data-service.service';
 import { Component, OnInit } from '@angular/core';
+import { Job } from '../job';
 
 @Component({
   selector: 'app-job',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private jobDataService: JobDataServiceService) { }
+
+  // Define a public property job and initial it to an empty list
+  jobs: Job[] = [];
+
+  public ngOnInit() {
+    this.jobDataService
+      .getAllJobs()
+      .subscribe(
+        jobs => this.jobs = jobs
+      );
   }
 
+  // Method to create a job triggered by an event
+  onAddJob(job: Job) {
+    this.jobDataService
+      .addJob(job)
+      .subscribe(
+        (newJob) => {
+          this.jobs = this.jobs.concat(newJob);
+        }
+      );
+  }
+
+  // Method to remove a job triggered by an event
+  onRemoveJob(job: Job) {
+    this.jobDataService
+      .deleteJobById(job.id)
+      .subscribe(
+        (_) => {
+          this.jobs = this.jobs.filter(
+            j => j.id !== job.id
+          );
+        }
+      );
+  }
+
+  // Method to change job availability triggered by an event
+  onToggleJobAvailability(job: Job) {
+    this.jobDataService
+      .toggleJobAvailability(job)
+      .subscribe(
+        updatedJob => job = updatedJob
+      );
+  }
 }
