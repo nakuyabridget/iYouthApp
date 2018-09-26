c651fab464f959e9cfab53a9606f81d3befa7eb0
oxenprogrammer <emmy1000okello@gmail.com>
2018-09-26 08:46:02 +0300

    Feature[#160618363 Fixes] add signup and login routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
+import { SignupComponent } from './auth/signup/signup.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { JobComponent } from './job/job.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { JobsResolver } from '../jobs.resolver';
+import { LoginComponent } from './auth/login/login.component';
 
 
 const routes: Routes = [
     {
         path: '',
-        redirectTo: 'jobs',
+        redirectTo: 'auth/signup',
         pathMatch: 'full'
     },
     {
@@ -17,6 +19,14 @@ const routes: Routes = [
         resolve: {
             jobs: JobsResolver
         }
+    },
+    {
+        path: 'auth/signup',
+        component: SignupComponent
+    },
+    {
+        path: 'auth/login',
+        component: LoginComponent
     },
     {
         path: '**',
